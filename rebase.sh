e13fa0e0c8e26d60a72ea960affe75aa740998a6
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-05-22 02:00:28 -0300

    Update rebase.sh

--- rebase.sh
+++ rebase.sh
@@ -2,4 +2,4 @@
 
 cd ~
 rm -rvf MasterNodes
-sudo apt install -y dialog && git clone https://github.com/celsoluism/MasterNodes.git && cd MasterNodes && chmod -R +x * && ./install.sh
+sudo apt install -y dialog && git clone https://github.com/celsoluism/MasterNodes.git && cd MasterNodes && chmod -R +x * && clear && ./install.sh
