763f4dadc12035f9edfc90a8c3830cca850773e8
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-05-23 20:32:32 -0300

    Update dextrocore_install.sh

--- dextrocore/dextrocore_install.sh
+++ dextrocore/dextrocore_install.sh
@@ -116,6 +116,8 @@ function prepare_node() { #TODO: add error detection
         #$COIN_TAR_UNZIP
         rm *.gz >/dev/null 2>&1
         rm *.zip >/dev/null 2>&1
+	   if [ ! -d "$TMP_FOLDER/installnode/$COIN_SUBFOLDER" ]; then cd $TMP_FOLDER/installnode/$COIN_SUBFOLDER ; fi
+	   if [ $? -ne 0 ]; then end; fi
 	strip $COIN_DAEMON $COIN_CLI $COIN_TX $COIN_QT
 	compile_error
 	chmod +x *
