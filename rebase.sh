844710e60311490529a9f34cf25a323d24081638
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-05-23 15:50:39 -0300

    Update rebase.sh

--- rebase.sh
+++ rebase.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+#Info: Install or Update MasterNode Daemons most automated possible, Masternode based on privkey.
+#PerfilConectado.NET MasterNodes Installer
+#TODO: to run you need to use ./rebase.sh .
+
+#SET COLORS
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
 cd ~
-sudo rm -rvf temp_masternodes
-sudo rm -rvf MasterNodes
-sudo apt install -y dialog 
+echo -q "${GREE}If prompted enter password to install dependences ${NC}"
+sudo rm -rvf temp_masternodes >/dev/null 2>&1
+sudo rm -rvf MasterNodes >/dev/null 2>&1
+sudo apt install -y dialog  >/dev/null 2>&1
 
 cd ~
-git clone https://github.com/celsoluism/MasterNodes.git
+git clone -q https://github.com/celsoluism/MasterNodes.git 
 cd MasterNodes
 chmod -R +x *
 
 clear
 
-./install.sh
+./install.sh 
