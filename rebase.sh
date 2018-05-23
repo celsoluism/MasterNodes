e9b4c5ed3e0246285e55c3424916585f14a75c9c
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-05-23 15:53:47 -0300

    Update rebase.sh

--- rebase.sh
+++ rebase.sh
@@ -8,17 +8,22 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+function dependences() {
 cd ~
 echo -q "${GREE}If prompted enter password to install dependences ${NC}"
 sudo rm -rvf temp_masternodes >/dev/null 2>&1
 sudo rm -rvf MasterNodes >/dev/null 2>&1
 sudo apt install -y dialog  >/dev/null 2>&1
+}
 
+function run_installer() {
 cd ~
 git clone -q https://github.com/celsoluism/MasterNodes.git 
 cd MasterNodes
 chmod -R +x *
-
 clear
-
 ./install.sh 
+}
+
+dependences
+run_installer
