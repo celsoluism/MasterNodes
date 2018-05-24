852a5019631041a7af1684fdd501599ce2fbf888
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-05-24 09:02:58 -0300

    Update omegacoin_install.sh

--- omegacoin/omegacoin_install.sh
+++ omegacoin/omegacoin_install.sh
@@ -168,7 +168,7 @@ function create_configs() {
         
 	rm $CONFIG_FOLDER/$CONFIG_FILE >/dev/null 2>&1
 	message "Creating $CONFIG_FILE..."
-	TEMPMNPRIVKEY="7ficcy6L4vnssPv38gUVubv6UKBGXQF24McozRAw7Zts7FkC91m"
+	TEMPMNPRIVKEY="5CA13pAP9TNTrQKVPLjY8ZuhDE7rZULf2tdv7Q3CC5uxtCjm3KY"
 		
 	if [ ! -d "$CONFIG_FOLDER" ]; then mkdir $CONFIG_FOLDER; fi
 	if [ $? -ne 0 ]; then error; fi
