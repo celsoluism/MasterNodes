6bd4aebeb434cf22b330fa459807e4d06fa8b52b
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-05-24 07:54:39 -0300

    Update omegacoin_menu.sh

--- omegacoin/omegacoin_menu.sh
+++ omegacoin/omegacoin_menu.sh
@@ -11,10 +11,11 @@ TITLE="Masternode Installer And Update"
 MENU="Choose one of the following options:"
 
 OPTIONS=(1 "Install Clean OmegaCoin"
-         2 "Update OmegaCoin"
-		 3 "Install Dependeces"
-         4 "Back to firs menu"
-         5 "Quit")
+         2 "Install Sentinel"
+	 4 "Update OmegaCoin"
+	 4 "Install Dependences"
+	 5 "Back to firs menu"
+         6 "Quit")
 
 CHOICE=$(dialog --clear \
                 --backtitle "$BACKTITLE" \
@@ -28,22 +29,26 @@ clear
 
 case $CHOICE in
         1)
-            echo "Install Clean OmegaCoin"
-            ./omegacoin/install_clean_omegacoin.sh
+            echo "Install Clean OmegaCoin MasterNode"
+            ./omegacoin/omegacoin_install.sh
             ;;
         2)
-            echo "Update OmegaCoin"
-            ./omegacoin/update_omegacoin.sh
+	    echo "Install Sentinel"
+            ./omegacoin/omegacoin_sentinel.sh
             ;;
-		3)  
-		    echo "Install Dependences"
-			./omegacoin/install_dependences_omegacoin.sh
-			;;
-        4)  
+	3
+            echo "Update OmegaCoin MasterNode"
+            ./omegacoin/omegacoin_update.sh
+            ;;
+	4)  
+	    echo "Install Dependences"
+	    ./omegacoin/omegacoin_dependences.sh
+	    ;;
+        5)  
             echo "You chose backup to first menu"
             ./install.sh
             ;;
-        5)  
+        6)  
             echo "You chose Quit!"
             echo "Access https://PerfilConectado.NET"
             echo "Thankyou!"
