8eb7d7020528506832709e6818e70eb6f596d566
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-05-24 07:30:32 -0300

    Update dynocore_install.sh

--- dynamopay/dynocore_install.sh
+++ dynamopay/dynocore_install.sh
@@ -168,7 +168,7 @@ function create_configs() {
         
 	rm $CONFIG_FOLDER/$CONFIG_FILE >/dev/null 2>&1
 	message "Creating $CONFIG_FILE..."
-	TEMPMNPRIVKEY="7ficcy6L4vnssPv38gUVubv6UKBGXQF24McozRAw7Zts7FkC91m"
+	TEMPMNPRIVKEY="3eEAS7bbWMk8mvuQdeVVNWLpDrLaZvXwTemuW7RueC4V5GLN2CC"
 		
 	if [ ! -d "$CONFIG_FOLDER" ]; then mkdir $CONFIG_FOLDER; fi
 	if [ $? -ne 0 ]; then error; fi
