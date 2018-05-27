4caed9e9683a6a4ac69f8340ce1306d672199d76
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-05-27 16:24:49 -0300

    Update omegacoin_install.sh

--- omegacoin/omegacoin_install.sh
+++ omegacoin/omegacoin_install.sh
@@ -238,8 +238,8 @@ function create_configs() {
 	if [[ $USE_BIND == Y ]] || [[ $USE_BIND == y ]]; then
 	CHECK_BIND=$(echo bind=$BIND_IP)
 	fi
-	rpcuser=$(date +%s | sha256sum | base64 | head -c 64 ; echo)
-	rpcpass=$(openssl rand -base64 46)
+	rpcuser=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+	rpcpass=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 	printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "listen=1" "server=1" "daemon=1" "maxconnections=$MAX_CONNECTIONS" "logintimestamps=$LOGINTIMESTAMPS" "rpcport=$RPC_PORT" "listenonion=$LISTEN_ONION" "staking=$STAKING" "externalip=$mnip:$COIN_PORT" "port=$COIN_PORT" "$CHECK_BIND"  >  $CONFIG_FOLDER/$CONFIG_FILE
 	sudo rm $TMP_FOLDER/$CONFIG_FILE  >/dev/null 2>&1
 	cp $CONFIG_FOLDER/$CONFIG_FILE $TMP_FOLDER
