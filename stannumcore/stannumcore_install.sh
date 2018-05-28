73225fe82db4097ee76611c733ca6291bbd2843d
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-05-27 22:00:34 -0300

    Update stannumcore_install.sh

--- stannumcore/stannumcore_install.sh
+++ stannumcore/stannumcore_install.sh
@@ -26,6 +26,7 @@ STAKING=0
 # CHECK BOLEANS
 USE_BIND=y
 USE_ADDR=y
+USE_SENTINEL=n
 
 # SENTINEL CONFIGURATIONS
 SENTINEL_REPO='https://github.com/omegacoinnetwork/sentinel.git'
@@ -528,7 +529,9 @@ install() {
 	create_configs
 	install_service
 	last_commits
+	if [[ $USE_SENTINEL == Y ]] || [[ $USE_SENTINEL == y ]]; then
 	install_sentinel
+	fi
 	success
 }
 
