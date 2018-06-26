04b631d91d4749ef4a81604ef703b58058e5433b
celsoluism <33703911+celsoluism@users.noreply.github.com>
2018-06-26 07:44:52 -0300

    Update brofist_install.sh

--- brofist/brofist_install.sh
+++ brofist/brofist_install.sh
@@ -6,7 +6,7 @@
 #--------------------------------------------- COIN INFORMATION --------------------------------------------
 # CONFIG ABOUT COIN
 COIN_NAME=brofist
-COLATERAL=1250PEW
+COLATERAL=ammount of PEW required by TIER
 CONFIG_FILE=brofist.conf
 
 # ALWAYS START WITH ~/ AND DEFAULT COIN FOLDER
